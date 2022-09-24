692b3afafe059c5709190aacd78c13d35e5cc36c
Asish George <asishag12@gmail.com>
2022-09-24 14:59:30 +0530

    formatting video.tsx

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,42 +1,60 @@
 import {useEffect, useCallback, useState} from 'react';
 import {continueRender, delayRender, Composition} from 'remotion';
 import {RedditTitleComp} from './components/RedditContent/RedditTitleComp';
-import {getRedditData} from './services/api';
-import {RedditThread} from './types/reddit_types';
+import {getAudioDurationInSeconds} from '@remotion/media-utils';
+let video_data = require('./resources/input.json');
+
 
 export const RemotionVideo: React.FC = () => {
 	const [data, setData] = useState({
-		thread: [],
-		base64_audio: [],
+		thread: video_data.thread,
+		base64_audio: video_data.base64_audio,
 	});
+	const [audioClipTime, setAudioClipTime] = useState([0]);
+	const [videoClipTime, setVideoClipTime] = useState([0]);
+	const [duration, setDuration] = useState(1);
+
 	const [handle] = useState(() => delayRender());
 
 	const fetchData = useCallback(async () => {
-		let d = new Date().getTime();
-		let thread = localStorage.getItem('thread');
-		if (!thread) {
-			try {
-				const response  = await getRedditData();
-				// you can find the sample response to the api in the sample_response.json file
-				
-				setData(response);
-				localStorage.setItem('thread', JSON.stringify(response));
-				console.log((new Date().getTime() - d) / 100);
-				continueRender(handle);
-			} catch (error) {
-				console.error(error);
-				continueRender(handle);
-			}
-		} else {
-			setData(JSON.parse(thread));
-			console.log((new Date().getTime() - d) / 100);
-			continueRender(handle);
-		}
-	}, [handle]);
+    let p: Array<Promise<number>> = [];
+		data.base64_audio.forEach((a: string) => {
+			p.push(getAudioDurationInSeconds(a));
+		});
+		Promise.all(p).then((time) => {
+			setAudioClipTime(time);
 
+			let videoClip: Array<number> = [];
+			let counter = 0;
+			data.thread.forEach((t: any) => {
+				let clip_len = 0;
+
+				for (let i = 0; i < t.audio.length; i++) {
+					clip_len = clip_len + time[counter + i];
+				}
+
+				counter += t.audio.length;
+				console.log('counter', counter);
+
+				videoClip.push(clip_len);
+			});
+			console.log('videoClip', videoClip);
+			console.log('time', time);
+
+			setVideoClipTime(videoClip);
+			let a = videoClip.reduce(
+				(previousValue, currentValue) => previousValue + currentValue,
+				0
+			);
+			console.log("duration", Math.round(a))
+			setDuration(Math.round(a))
+			continueRender(handle);
+		});
+  }, [handle]);
+
+	
 	useEffect(() => {
 		fetchData();
-		console.log('sdfsd');
 	}, [fetchData]);
 
 	return (
@@ -46,7 +64,7 @@ export const RemotionVideo: React.FC = () => {
 				// npx remotion render src/index.tsx <id> out/video.mp4
 				id="RedditTitleComp"
 				component={RedditTitleComp}
-				durationInFrames={1800}
+				durationInFrames={!duration || duration > 1800 ? 1800 : duration * 30}
 				fps={30}
 				width={1080}
 				height={1920}
@@ -56,6 +74,8 @@ export const RemotionVideo: React.FC = () => {
 					thread: data.thread,
 					audio: data.base64_audio,
 					clip_durations: [],
+					audio_clip_time: audioClipTime,
+					video_clip_time: videoClipTime,
 				}}
 			/>
 		</>
